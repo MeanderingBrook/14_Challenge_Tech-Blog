3e07cba492451b48149d24db2c3b794eed9b4212
Meandering Brook <jaguar_rebuke_0g@icloud.com>
2024-09-06 13:12:43 -0400

    Misc Connection Update

--- config/connection.js
+++ config/connection.js
@@ -9,12 +9,12 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
-    "techblog_db",
-    "postgres",
-    "password",
-    // process.env.DB_NAME,
-    // process.env.DB_USER,
-    // process.env.DB_PASSWORD,
+    // "techblog_db",
+    // "postgres",
+    // "password",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: "localhost",
       dialect: "postgres",
